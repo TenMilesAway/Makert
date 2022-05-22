37d4b2af71564dc5275c5f3cdda43029774b0a8a
TenMilesAway <94096304+TenMilesAway@users.noreply.github.com>
2022-05-22 21:07:22 +0800

    Add files via upload

---
+++ market-vue/src/src/components/MarketHeader.vue
@@ -0,0 +1,128 @@
+<template>
+  <!-- 头部 -->
+    <div class="login-header">
+      <div class="login-header-logo">
+        <router-link to="/home">
+          <img src="@/assets/logo.png" alt="">
+          <h3>B&nbspI&nbspS&nbspT&nbspU</h3>
+        </router-link>
+      </div>
+      <div class="login-header-navigation">
+        <div class="login-header-navigation-items">
+          <ul>
+            <li>
+              <router-link to="/home">
+                商城主页
+              </router-link>
+            </li>
+            <li>我的店铺</li>
+            <li>我的订单</li>
+            <li>购物车</li>
+          </ul>
+        </div>
+        <div class="login-header-navigation-headPortrait"></div>
+      </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MarketHeader',
+}
+</script>
+
+<style scoped>
+  /* router-link 移入动画 */
+  @keyframes fontConversionAnimation {
+    0% {
+      font-weight: 700;
+    }
+    30% {
+      font-weight: 800;
+    }
+    60% {
+      font-weight: 900;
+    }
+    100% {
+      font-weight: 1000;
+      color: #81cdf6;
+    }
+  }
+
+  li {
+    display: inline-block;
+    list-style: none;
+  }
+  /* 头部模块 */
+  .login-header {
+    position: relative;
+    left: 0;
+    min-height: 50px;
+    background-color: #0086CF;
+  }
+
+  .login-header .login-header-logo {
+    width: 200px;
+    height: 50px;
+  }
+
+  .login-header .login-header-logo img {
+    vertical-align: middle;
+    width: 50px;
+    margin-left: 40px;
+    margin-top: -15px;
+  }
+
+  .login-header .login-header-logo h3 {
+    display: inline-block;
+    line-height: 50px;
+    padding-left: 5px;
+    color: white;
+    font-size: 24px;
+  }
+
+  .login-header .login-header-navigation {
+    position: absolute;
+    right: 20px;
+    top: 5px;
+    width: 450px;
+    height: 40px;
+    background-color: rgb(79, 143, 216);
+    border-radius: 5px;
+  }
+
+  .login-header .login-header-navigation .login-header-navigation-items {
+    position: absolute;
+    display: flex;
+    justify-content: space-around;
+    left: 10px;
+    top: 2px;
+    width: 400px;
+    height: 36px;
+    line-height: 36px;
+  }
+
+  .login-header .login-header-navigation .login-header-navigation-items li {
+    width: 90px;
+    color: #eee;
+  }
+  .login-header .login-header-navigation .login-header-navigation-items li>a {
+    text-decoration: none;
+    color: #eee;
+  }
+
+  .login-header .login-header-navigation .login-header-navigation-items li>a:hover {
+    animation: fontConversionAnimation 0.15s linear forwards;
+  }
+
+  .login-header .login-header-navigation .login-header-navigation-headPortrait {
+    position: absolute;
+    right: 25px;
+    top: 2px;
+    display: inline-block;
+    width: 36px;
+    height: 36px;
+    border-radius: 18px;
+    background-color: #000;
+  }
+</style>
---
+++ market-vue/src/src/views/Login.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="login-container">
+    <!-- 头部 -->
+    <market-header></market-header>
+
+    <!-- 主体 -->
+    <div class="login-body">
+      <!-- 左侧登录框 -->
+      <div class="login-body-box">
+        <div class="login-body-reg">
+          <router-link to="/reg">
+            <img src="../assets/reg_icon.png" alt="">
+          </router-link>
+        </div>
+        <div class="login-body-window">
+          <div class="login-body-headtext">
+            <h3>账号登陆</h3>
+            <h4>每一天，欢乐充斥校园</h4>
+          </div>
+          <el-form ref="logform" label-width="80px">
+            <el-form-item label="用户名：">
+              <el-input v-model="userAccount" style="width: 280px"></el-input>
+            </el-form-item>
+            <el-form-item label="密码：">
+              <el-input v-model="userPwd" style="width: 280px"></el-input>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" style="margin-left: 45px;margin-top: 50px">登陆</el-button>
+              <el-button type="info">重置</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </div>
+      <!-- 右侧背景图 -->
+      <div class="login-body-imgbox"></div>
+    </div>
+
+    <!-- 底部 -->
+    <div class="login-footer">
+
+    </div>
+  </div>
+</template>
+
+<script>
+import MarketHeader from '@/components/MarketHeader.vue'
+
+export default {
+  name: 'Login',
+  data() {
+    return {
+      labelPosition: 'left',
+      userAccount: '',
+      userPwd: ''
+    }
+  },
+  components: {
+    MarketHeader
+  }
+}
+</script>
+
+<style>
+  * {
+    padding: 0px;
+    margin: 0px;
+  }
+
+  /* 登录框背景动画 */
+  @keyframes logWindowBgcMove {
+    0%, 100% {
+      background-position-y: -50px;
+      background-position-x: 0px;
+    }
+    10%, 90% {
+      background-position-y: -90px;
+      background-position-x: -20px;
+
+    }
+    20%, 80% {
+      background-position-y: -180px;
+      background-position-x: -40px;
+    }
+    30%, 70% {
+      background-position-y: -270px;
+      background-position-x: -60px;
+    }
+    40%, 60% {
+      background-position-y: -360px;
+      background-position-x: -80px;
+    }
+    50% {
+      background-position-y: -400px;
+      background-position-x: -100px;
+    }
+  }
+
+  /* 主体模块 */
+  .login-body {
+    display: flex;
+    height: 640px;
+  }
+
+  .login-body .login-body-box {
+    position: relative;
+    overflow: hidden;
+    width: 500px;
+    height: 100%;
+    background-image: url('../assets/background_1.png');
+    background-repeat: no-repeat;
+    background-size: 600px;
+    animation: logWindowBgcMove 10s linear infinite;
+  }
+
+  .login-body .login-body-box .login-body-reg {
+    position: absolute;
+    left: 410px;
+    top: 130px;
+    width: 40px;
+    height: 22px;
+    border-top-left-radius: 5px;
+    border-bottom-left-radius: 5px;
+    background-color: rgb(145, 128, 229);
+    box-shadow: 2px 2px 3px 0px rgb(108, 81, 81);
+  }
+
+  .login-body .login-body-box .login-body-reg img {
+    width: 40px;
+  }
+  
+  .login-body .login-body-box .login-body-window {
+    height: 540px;
+    width: 80%;
+    margin-top: 50px;
+    margin-left: 10%;
+    background-color: #5BD2F5;
+    border-radius: 20px;
+  }
+
+  .login-body .login-body-box .login-body-window .login-body-headtext {
+    width: 100%;
+    padding-top: 20px;
+    padding-bottom: 10px;
+    padding-left: 20px;
+    font-size: 25px;
+    color: #eee;
+  }
+
+  .login-body .login-body-box .login-body-window .login-body-headtext h4 {
+    padding-top: 10px;
+    padding-bottom: 60px;
+  }
+
+  .login-body .login-body-box .login-body-window hr {
+    margin-bottom: 40px;
+  }
+
+  .login-body .login-body-imgbox {
+    flex: 1;
+    background-image: url('../assets/img_3.webp');
+    background-repeat: no-repeat;
+    background-size: 1100px;
+  }
+
+  /* 底部模块 */
+  .login-footer {
+    height: 80px;
+    width: 100%;
+    background-color: #0086CF;
+  }
+</style>
